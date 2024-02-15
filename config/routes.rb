597e403849219f2802e2a01c4c16348c0e2ab4a5
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-15 18:21:28 +0100

    Set tasks#index as the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :tasks
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "tasks#index"
 end
