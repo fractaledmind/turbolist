8e2226fc842a95c8260120dfd2c56ebc584824a2
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-15 18:43:54 +0100

    Add an icon helper that uses the Bootstrap icons SVG sprite file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def icon(name, classes: nil)
+    tag.svg class: class_names("bi bi--#{name}", classes), fill: "currentColor", width: 16, height: 16, viewPath: "0 0 16 16" do
+      tag.use nil, 'xlink:href': "/bootstrap-icons.svg##{name}", role: :presentation
+    end
+  end
 end
