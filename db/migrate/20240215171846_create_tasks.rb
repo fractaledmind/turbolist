c97994a54c7f3684e9e875e6b738e401358f695c
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-15 18:19:58 +0100

    Ensure Task#title is always present

--- db/migrate/20240215171846_create_tasks.rb
+++ db/migrate/20240215171846_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[7.2]
   def change
     create_table :tasks do |t|
-      t.string :title
+      t.string :title, null: false
 
       t.timestamps
     end
