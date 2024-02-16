4e67a6d17b7daa626cf3f42c07212423822074d5
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-16 19:44:39 +0100

    Allow button_to form to toggle Task#completed_at

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,6 +54,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title)
+      params.require(:task).permit(:title, :completed)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+
+  def completed? = completed_at.present?
+
+  def completed=(_value)
+    if completed_at.present?
+      self.completed_at = nil
+    else
+      self.completed_at = Time.current
+    end
+  end
 end
