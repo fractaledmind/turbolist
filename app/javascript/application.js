ce8bca8555eef9fe0580650be2a98bb112e60499
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-18 00:13:18 +0100

    Scope the sliding view transition animation to a specific navigation type
    from Tasks#index to Tasks#show

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+navigation.addEventListener('navigate', (event) => {
+  const toUrl = new URL(event.destination.url);
+  const fromPath = location.pathname;
+
+  console.log(toUrl.pathname, fromPath)
+  document.documentElement.dataset.fromPath = fromPath;
+  document.documentElement.dataset.toPath = toUrl.pathname;
+});
