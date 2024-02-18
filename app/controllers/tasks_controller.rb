d9c4e22b6f2eaf8d3c5748dff24c6d862b92ef9d
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-18 01:55:10 +0100

    Ensure that the Tasks controller permits Task#labels params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -56,6 +56,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :description, :completed)
+      params.require(:task).permit(:title, :description, :completed, labels: [])
     end
 end
