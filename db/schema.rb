6e13f82ca9e9931d4a133a8171ed972dcd1d534c
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-18 01:05:06 +0100

    Setup ActionText
    $ bundle install
    
    Fetching gem metadata from https://rubygems.org/...........
    Resolving dependencies...
    Fetching ffi 1.16.3
    Fetching mini_magick 4.12.0
    Installing mini_magick 4.12.0
    Installing ffi 1.16.3 with native extensions
    Fetching ruby-vips 2.2.0
    Installing ruby-vips 2.2.0
    Fetching image_processing 1.12.2
    Installing image_processing 1.12.2
    Bundle complete! 18 Gemfile dependencies, 102 gems now installed.
    Use `bundle info [gemname]` to see where a bundled gem is installed.
    
    $ bin/rails db:migrate
    == 20240218000205 CreateActiveStorageTables: migrating ========================
    -- create_table(:active_storage_blobs, {:id=>:primary_key})
       -> 0.0024s
    -- create_table(:active_storage_attachments, {:id=>:primary_key})
       -> 0.0019s
    -- create_table(:active_storage_variant_records, {:id=>:primary_key})
       -> 0.0009s
    == 20240218000205 CreateActiveStorageTables: migrated (0.0053s) ===============
    
    == 20240218000206 CreateActionTextTables: migrating ===========================
    -- create_table(:action_text_rich_texts, {:id=>:primary_key})
       -> 0.0066s
    == 20240218000206 CreateActionTextTables: migrated (0.0067s) ==================

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_02_16_180832) do
+ActiveRecord::Schema[7.2].define(version: 2024_02_18_000206) do
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", null: false
@@ -18,4 +56,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_02_16_180832) do
     t.datetime "completed_at"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
