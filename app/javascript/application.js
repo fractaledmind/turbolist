9b0ae220610961ba1c14c73b9c6e98acac0031e7
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-19 21:54:51 +0100

    Add a keyboard shortcut to submit the Task#description rich text field form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -13,3 +13,11 @@ navigation.addEventListener('navigate', (event) => {
 
 import "trix"
 import "@rails/actiontext"
+
+const element = document.querySelector("trix-editor")
+element.addEventListener("keydown", event => {
+  if (event.key == "Enter" && event.metaKey) {
+    event.preventDefault()
+    element.inputElement.form.requestSubmit()
+  }
+})
