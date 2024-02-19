83095e4b014ad6c41a819e9badca1de3d253f40b
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-19 22:42:56 +0100

    Run migration
    $ bin/rails db:migrate
    == 20240219214125 CreateLists: migrating ======================================
    -- create_table(:lists)
       -> 0.0019s
    == 20240219214125 CreateLists: migrated (0.0020s) =============================

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_02_18_003030) do
+ActiveRecord::Schema[7.2].define(version: 2024_02_19_214125) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -47,6 +47,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_02_18_003030) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
