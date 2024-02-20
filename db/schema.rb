52ba4dd3a8ca2ea1e2d0c53592005877609a8e53
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-20 20:57:47 +0100

    Run the migration
    $ bin/rails db:migrate
    == 20240220195540 AddAvatarToLists: migrating =================================
    -- add_column(:lists, :avatar, :string)
       -> 0.0015s
    == 20240220195540 AddAvatarToLists: migrated (0.0016s) ========================

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_02_19_214125) do
+ActiveRecord::Schema[7.2].define(version: 2024_02_20_195540) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_02_19_214125) do
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
   create_table "tasks", force: :cascade do |t|
