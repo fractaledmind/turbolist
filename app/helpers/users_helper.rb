9fc4a1ed3411c29deccf62dec176ef63849252e3
Stephen Margheim <stephen.margheim@gmail.com>
2024-02-24 14:38:08 +0100

    Show basic User generated avatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def avatar_for(email, size_class: "size-4")
+    tag.span class: "inline-flex #{size_class} items-center justify-center rounded-full bg-blue-500" do
+      tag.span email[0].upcase, class: "text-xs leading-none text-white"
+    end
+  end
 end
